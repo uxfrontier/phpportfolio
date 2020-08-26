d30be39ba5e4b3f88092037fa749df5e9ff55cf2
uxfrontier <devjourney@icloud.com>
2020-08-27 00:37:02 +0100

    landing-page commit

--- landing-page.php
+++ landing-page.php
@@ -2,27 +2,25 @@
 <div class="jumbotron text-center" id="intro">
   <h1 class="display-4">Hello, world! </h1>
   <p class="lead">I am Adam, and this is the beggining of my journey <br>into <strong>Web Development</strong>.
-  <br>
-  I enjoy learning about all the latest web technologies and trends. My aim by the end of next year is to 
-  gain employment as a php developer. This portfolio is to show my learning progress 
+ 
   </p>
   <hr class="my-4">
-  <a class="btn btn-dark btn-sm" href="#about-me" role="button">About Me</a>
-  <p class="mt-4 text-secondary">This project was started on the <?php echo date("d-m-y") . " " . "at 5.58pm"; ?></p>
+  <a class="btn btn-dark btn-lg" href="#about-me" role="button">About Me</a>
+  <p class="mt-4 text-secondary"><small>This project was started on the <?php echo date("d-m-y") . " " . "at 5.58pm"; ?></small></p>
 </div>
 
-<div class="container" id="about-me">
+<div class="container-fluid" id="about-me">
     <h1 class="text-center">About me</h1>
     <div class="row">
         
     <div class=" container col-sm-6 text-center" id="about-col-1">
-        <p>I am a 34 year old aspiring web developer based in Leicestershire, UK.</p>
+        <p id="lead-p" class="pt-4 text-center">I am a 34 year old aspiring web developer based in Leicestershire, UK.
+        I enjoy learning about all the latest web technologies and trends. My aim by the end of next year is to 
+  gain employment as a php developer. This portfolio is the start of my learning progress.
+        </p>
     </div>
 
     <div class="container col-sm-6 text-center" id="about-col-2">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Sint, odio facilis! Et odit, non aut voluptates incidunt voluptatum 
-        a delectus?
-        </p>
+       <img src="./images/sphere.png" alt="sphere" id="sphere">
     </div>
 </div>
